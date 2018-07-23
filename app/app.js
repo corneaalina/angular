15d3fc4812f5143d9cbb97ca681bab4a76745424
Alina Cornea <alina.cornea@pitechnologies.ro>
2018-07-23 14:06:14 +0300

    Adds the phoneList component.

--- app/app.js
+++ app/app.js
@@ -1,18 +1,2 @@
 // Defihnes the ponecatApp module.
-var phonecatApp = angular.module('phonecatApp', []);
-
-// Defines the PhoneListController controller on the phonecatApp module.
-phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-    $scope.phones = [
-        {
-            name: 'Nexus S',
-            snippet: 'Fast just got faster with Nexus S.'
-        }, {
-            name: 'Motorola XOOM™ with Wi-Fi',
-            snippet: 'The Next, Next Generation tablet.'
-        }, {
-            name: 'MOTOROLA XOOM™',
-            snippet: 'The Next, Next Generation tablet.'
-        },
-    ];
-});
+angular.module('phoneModule', []);
